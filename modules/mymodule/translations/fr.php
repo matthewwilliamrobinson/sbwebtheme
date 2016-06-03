50722297ecc072959f70400ff41592e5a75a524b
Samuel Phillips <samuel.phillips@sbwebstore.com>
2016-06-03 19:01:11 +0000

    Copied all of default-bootstrap into sbwebtheme

---
+++ modules/mymodule/translations/fr.php
@@ -0,0 +1,21 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{mymodule}default-bootstrap>mymodule_2ddddc2a736e4128ce1cdfd22b041e7f'] = 'Mon module';
+$_MODULE['<{mymodule}default-bootstrap>mymodule_d6968577f69f08c93c209bd8b6b3d4d5'] = 'Description du module.';
+$_MODULE['<{mymodule}default-bootstrap>mymodule_876f23178c29dc2552c0b48bf23cd9bd'] = 'Êtes-vous certain de vouloir désinstaller ce module ? Vous perdrez tous vos réglages !';
+$_MODULE['<{mymodule}default-bootstrap>mymodule_0f40e8817b005044250943f57a21c5e7'] = 'Aucun nom fourni';
+$_MODULE['<{mymodule}default-bootstrap>mymodule_36da21295037ed79b11a4bd840a8b86f'] = 'Il s\'agit d\'un simple message texte';
+$_MODULE['<{mymodule}default-bootstrap>mymodule_fe5d926454b6a8144efce13a44d019ba'] = 'Valeur de configuration non valide.';
+$_MODULE['<{mymodule}default-bootstrap>mymodule_c888438d14855d7d96a2724ee9c306bd'] = 'Réglages mis à jour';
+$_MODULE['<{mymodule}default-bootstrap>mymodule_f4f70727dc34561dfde1a3c529b6205c'] = 'Réglages';
+$_MODULE['<{mymodule}default-bootstrap>mymodule_2f6e771db304264c8104cb7534bb80cd'] = 'Valeur de configuration';
+$_MODULE['<{mymodule}default-bootstrap>mymodule_c9cc8cce247e49bae79f15173ce97354'] = 'Enregistrer';
+$_MODULE['<{mymodule}default-bootstrap>mymodule_630f6dc397fe74e52d5189e2c80f282b'] = 'Retour à la liste';
+$_MODULE['<{mymodule}default-bootstrap>display_86e88cbccafa83831b4c6685501c6e58'] = 'Bienvenue à cette page!';
+$_MODULE['<{mymodule}default-bootstrap>mymodule_9a843f20677a52ca79af903123147af0'] = 'Bienvenue !';
+$_MODULE['<{mymodule}default-bootstrap>mymodule_f5a7924e621e84c9280a9a27e1bcb7f6'] = 'Monde';
+$_MODULE['<{mymodule}default-bootstrap>mymodule_3af204e311ba60e6556822eac1437208'] = 'Bonjour %s !';
+$_MODULE['<{mymodule}default-bootstrap>mymodule_c66b10fbf9cb6526d0f7d7a602a09b75'] = 'Cliquez sur ce lien';
+$_MODULE['<{mymodule}default-bootstrap>mymodule_f42c5e677c97b2167e7e6b1e0028ec6d'] = 'Cliquez-moi !';
